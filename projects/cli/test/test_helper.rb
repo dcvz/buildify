f4369a41cdbd2977979688577a7099ae1f442d73
Pedro Piñera <pedro@ppinera.es>
2021-05-04 18:50:25 +0200

    Remove the CLI

--- projects/cli/test/test_helper.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
-require "buildify"
-require "minitest/reporters"
-
-Minitest::Reporters.use! Minitest::Reporters::JUnitReporter.new
-
-require "minitest/autorun"
