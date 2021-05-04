9a83bef52aec08c2895ce818a69dcd6601c64b0c
Pedro Piñera <pedro@ppinera.es>
2021-05-04 18:46:56 +0200

    Share Rubocop config

--- projects/toolkit/lib/toolkit/services/test/toolkit.rb
+++ projects/toolkit/lib/toolkit/services/test/toolkit.rb
@@ -9,29 +9,29 @@ module Toolkit
         attr_reader :test
 
         def initialize(test: nil)
+          super
           @test = test
         end
 
         def call
           Dir.chdir(Constants::ROOT_DIRECTORY) do
-            lib_directory = File.expand_path("lib", Constants::TOOLKIT_DIRECTORY)
-            test_directory = File.expand_path("test", Constants::TOOLKIT_DIRECTORY)
-
-            test_paths = if @test.nil?
-              Dir.glob(File.join(Constants::TOOLKIT_DIRECTORY, "test/**/*_test.rb"))
-            else
-              @test
-            end
-
-            arguments = [
-              "ruby",
-              "-I#{lib_directory}",
-              "-I#{test_directory}",
-              "-e \"ARGV.each {|f| require f}\"",
-              *test_paths,
-            ].join(" ")
-            Utilities::System.system(arguments)
+            Utilities::System.system(arguments.join(" "))
           end
+        end
+
+        private
+
+        def arguments
+          lib_directory = File.expand_path("lib", Constants::TOOLKIT_DIRECTORY)
+          test_directory = File.expand_path("test", Constants::TOOLKIT_DIRECTORY)
+          test_paths = if @test.nil?
+                         Dir.glob(File.join(Constants::TOOLKIT_DIRECTORY, "test/**/*_test.rb"))
+                       else
+                         @test
+                       end
+          [
+            "ruby", "-I#{lib_directory}", "-I#{test_directory}", "-e \"ARGV.each {|f| require f}\"", *test_paths
+          ]
         end
       end
     end
