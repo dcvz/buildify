56bbc6ba1f138ffb1a2e3b9985a52eb39111c1a0
Pedro Piñera <pepibumur@gmail.com>
2021-05-10 17:24:51 +0200

    Set up documentation

--- projects/docs/docusaurus.config.js
+++ projects/docs/docusaurus.config.js
@@ -1,100 +1,102 @@
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
+const remarkEmoji = require("remark-emoji");
+const remarkExternalLinks = require("remark-external-links");
+
 module.exports = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  title: "Buildify Documentation",
+  tagline: "Documentation about how to use and contribute to the tool.",
+  url: "https://dev.buildify.dev",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "buildifydev",
+  projectName: "buildify",
+  customFields: {
+    defaultDocsLandingPage: "tutorial/get-started",
+  },
   themeConfig: {
+    prism: {
+      additionalLanguages: ["ruby", "typescript", "javascript"],
+      theme: require("prism-react-renderer/themes/dracula"),
+    },
+    algolia: {
+      apiKey: "dev",
+      indexName: "dev",
+      contextualSearch: true,
+      searchParameters: {},
+    },
     navbar: {
-      title: 'My Site',
+      title: "Buildify",
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: "Buildify Logo",
+        src: "img/logo.svg",
       },
       items: [
         {
-          type: 'doc',
-          docId: 'intro',
-          position: 'left',
-          label: 'Tutorial',
-        },
-        {to: '/blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/buildifydev/buildify",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Docs',
+          title: "Docs",
           items: [
             {
-              label: 'Tutorial',
-              to: '/docs/intro',
+              label: "Get Started",
+              to: "/",
             },
           ],
         },
         {
-          title: 'Community',
+          title: "Community",
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              label: "GitHub Discussions",
+              href: "https://github.com/buildifydev/buildify/discussions",
             },
             {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
+              label: "Slack",
+              href:
+                "https://join.slack.com/t/buildifydev/shared_invite/zt-q1ab0kza-nFfA1pLCkafi3_cU85yIRQ",
             },
             {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
+              label: "Twitter",
+              href: "https://twitter.com/buildifydev",
             },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              label: "buildify.dev",
+              to: "https://buildify.dev",
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Buildify, Inc. Built with Docusaurus.`,
     },
   },
+  plugins: [],
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
+          routeBasePath: "/",
+          sidebarPath: require.resolve("./sidebars.js"),
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            "https://github.com/buildifydev/buildify/blob/main/projects/docs/",
+          remarkPlugins: [remarkEmoji, remarkExternalLinks],
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
