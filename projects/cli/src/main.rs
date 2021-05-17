66a9aa8f3b3176a6ca4d28964b3d66749a7ef448
David Chavez <david@dcvz.io>
2021-05-17 16:26:57 +0200

    Add basic CLI with init command

--- projects/cli/src/main.rs
+++ projects/cli/src/main.rs
@@ -1,3 +1,37 @@
+extern crate clap;
+
+use clap::{AppSettings, Clap};
+
+#[derive(Clap)]
+#[clap(version = "0.0.1")]
+#[clap(author = "David C. <david@dcvz.io>")]
+#[clap(about = "Buildify CLI interface for managing projects")]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct CLI {
+    #[clap(subcommand)]
+    subcommand: SubCommand
+}
+
+#[derive(Clap)]
+enum SubCommand {
+    #[clap(version = "0.0.1", about = "Bootstraps a project", author = "David C. <david@dcvz.io>")]
+    Init(Init),
+}
+
+#[derive(Clap)]
+struct Init {
+    #[clap(short = 'n', long = "name", about = "The name of the project", required = true)]
+    #[clap(value_name = "PROJECT_NAME")]
+    name: String
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli: CLI = CLI::parse();
+
+    match cli.subcommand {
+        SubCommand::Init(i) => {
+            let project_name = i.name;
+            println!("Should init project with name: {}", project_name)
+        }
+    }
 }
