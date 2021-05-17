a2b3d2194fec6e5e1d5237f8b9d46cef29264cfd
David Chavez <david@dcvz.io>
2021-05-17 17:37:01 +0200

    Add tests

--- projects/cli/src/main.rs
+++ projects/cli/src/main.rs
@@ -2,7 +2,7 @@ extern crate clap;
 
 use clap::{AppSettings, Clap};
 
-#[derive(Clap)]
+#[derive(Clap, PartialEq, Debug)]
 #[clap(version = "0.0.1")]
 #[clap(author = "David C. <david@dcvz.io>")]
 #[clap(about = "Buildify CLI interface for managing projects")]
@@ -12,13 +12,13 @@ struct CLI {
     subcommand: SubCommand
 }
 
-#[derive(Clap)]
+#[derive(Clap, PartialEq, Debug)]
 enum SubCommand {
     #[clap(version = "0.0.1", about = "Bootstraps a project", author = "David C. <david@dcvz.io>")]
     Init(Init),
 }
 
-#[derive(Clap)]
+#[derive(Clap, PartialEq, Debug)]
 struct Init {
     #[clap(short = 'n', long = "name", about = "The name of the project", required = true)]
     #[clap(value_name = "PROJECT_NAME")]
@@ -33,5 +33,48 @@ fn main() {
             let project_name = i.name;
             println!("Should init project with name: {}", project_name)
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_no_cmd() {
+        CLI::try_parse_from(&["test"]).unwrap_err();
+    }
+
+    #[test]
+    fn test_init_noname() {
+        CLI::try_parse_from(&["test", "init"]).unwrap_err();
+    }
+
+    #[test]
+    fn test_init_long() {
+        assert_eq!(
+            CLI {
+                subcommand: SubCommand::Init(
+                    Init {
+                        name: "test".to_string()
+                    }
+                )
+            },
+            CLI::parse_from(&["test", "init", "--name", "test"])
+        )
+    }
+
+    #[test]
+    fn test_init_short() {
+        assert_eq!(
+            CLI {
+                subcommand: SubCommand::Init(
+                    Init {
+                        name: "test".to_string()
+                    }
+                )
+            },
+            CLI::parse_from(&["test", "init", "-n", "test"])
+        )
     }
 }
